03883086561fa315ed5fa67e2f83ec1d8bf3c552
guilherssousa <guilherssousa@gmail.com>
2022-03-11 19:51:39 -0300

    transform PopularDramas in Widget Component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import { ReactChild } from 'react'
 import { Drama } from 'types'
 
+import PopularDramas from 'components/PopularDramas'
 interface LayoutProps {
   topDramas: Drama[]
   children: ReactChild
@@ -12,38 +13,7 @@ const Layout: NextPage<LayoutProps> = ({ topDramas, children }) => {
     <div className={'flex'}>
       <main className={'w-full p-4 md:w-8/12'}>{children}</main>
       <aside className={'hidden p-4 sm:w-4/12 md:block'}>
-        <div className="border p-4">
-          <span className={'font-bold uppercase text-blue-400'}>
-            Dramas populares
-          </span>
-          <div className={'mt-2'}>
-            {topDramas.map((drama) => (
-              <div
-                key={`top-dramas-${drama.slug_query}`}
-                className={'group mt-3 flex cursor-pointer'}
-              >
-                <div className={'w-20'}>
-                  <img
-                    src={drama.data.poster}
-                    className={'w-20 object-cover'}
-                  ></img>
-                </div>
-                <div className={'ml-2 w-7/12'}>
-                  <span
-                    className={
-                      'block text-sm font-bold text-blue-900 group-hover:text-blue-400'
-                    }
-                  >
-                    {drama.data.title}
-                  </span>
-                  <span className={'text-sm'}>
-                    {drama.data.details.episodes} espisódios
-                  </span>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <PopularDramas topDramas={topDramas} />
       </aside>
     </div>
   )
---
+++ src/components/PopularDramas/index.tsx
@@ -0,0 +1,45 @@
+import { NextPage } from 'next'
+import { Drama } from 'types'
+
+interface LayoutProps {
+  topDramas: Drama[]
+}
+
+const PopularDramas: NextPage<LayoutProps> = ({ topDramas }) => {
+  return (
+    <div className="border p-4">
+      <span className={'font-bold uppercase text-blue-400'}>
+        Dramas populares
+      </span>
+      <div className={'mt-2'}>
+        {topDramas.map((drama) => (
+          <div
+            key={`top-dramas-${drama.slug_query}`}
+            className={'group mt-3 flex cursor-pointer'}
+          >
+            <div className={'w-20'}>
+              <img
+                src={drama.data.poster}
+                className={'w-20 object-cover'}
+              ></img>
+            </div>
+            <div className={'ml-2 w-7/12'}>
+              <span
+                className={
+                  'block text-sm font-bold text-blue-900 group-hover:text-blue-400'
+                }
+              >
+                {drama.data.title}
+              </span>
+              <span className={'text-sm'}>
+                {drama.data.details.episodes} espisódios
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default PopularDramas
